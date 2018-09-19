5cd2eeb9abdff1557890c611214add2fdff083b7
AugustoChies <guto_chies@hotmail.com>
2018-09-19 01:20:01 -0300

    first commit

---
+++ Assets/MeshInfoColector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshInfoColector : MonoBehaviour {
+    public MeshFilter mf;
+    public Vector3[] v;
+
+	// Use this for initialization
+	void Start () {
+        mf = this.gameObject.GetComponent<MeshFilter>();
+        v = mf.mesh.vertices;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
