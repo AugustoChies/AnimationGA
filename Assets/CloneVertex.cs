db34205239b7b814c8489c6258b8b76583e270c5
AugustoChies <guto_chies@hotmail.com>
2018-10-10 09:13:12 -0300

    added mesh clones

---
+++ Assets/CloneVertex.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneVertex : MonoBehaviour {
+    public GameObject original;
+    public MeshFilter mf;
+
+	// Use this for initialization
+	void Start () {
+        mf = this.gameObject.GetComponent<MeshFilter>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        mf.mesh.SetVertices(original.GetComponent<MeshInfoColector>().newv);
+	}
+}
